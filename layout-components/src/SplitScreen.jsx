634169db5c588153b9c8eda0cdc8bbd53572d5b9
Malik Shalash <mjshalash@rci4u.com>
2025-11-09 22:00:35 -0500

    Timestamp 4:02 - WIP

--- layout-components/src/SplitScreen.jsx
+++ layout-components/src/SplitScreen.jsx
@@ -2,22 +2,18 @@ import styled from 'styled-components';
 
 const Container = styled.div`
   display: flex;
-`
+`;
 
 const Pane = styled.div`
-  flex: ${props => props.weight};
-`
+  flex: ${(props) => props.weight};
+`;
 
 export const SplitScreen = ({ leftWeight = 1, rightWeight = 1, children }) => {
-  const [left, right] = children
+  const [left, right] = children;
   return (
     <Container>
-      <Pane weight={leftWeight}>
-        {left}
-      </Pane>
-      <Pane weight={rightWeight}>
-        {right}
-      </Pane>
+      <Pane weight={leftWeight}>{left}</Pane>
+      <Pane weight={rightWeight}>{right}</Pane>
     </Container>
   );
-}
+};
